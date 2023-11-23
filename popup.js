21fd5396d0160e0d4610558d46e43cb208317d62
Harsh Buddhdev <harshbuddhdev5@.com>
2023-11-23 10:38:19 -0500

    working for Linkedin

--- popup.js
+++ popup.js
@@ -3,15 +3,15 @@
 function addEyeSymbol(link) {
   const eyeSymbol = document.createElement('div');
   eyeSymbol.innerHTML = '👁️'; // You can customize the symbol
-  // eyeSymbol.style.position = 'absolute';
+  eyeSymbol.style.position = 'absolute';
 
   // Adjust the position a bit higher (e.g., 10 pixels)
   // eyeSymbol.style.top = link.offsetTop - 10+ 'px';
 
-  // eyeSymbol.style.left = link.offsetLeft + link.offsetWidth + 10 + 'px';
+  eyeSymbol.style.left = link.offsetLeft + link.offsetWidth + 10 + 'px';
   eyeSymbol.style.fontSize = '20px';
   eyeSymbol.style.cursor = 'pointer';
-  // eyeSymbol.style.zIndex = '9999';
+  eyeSymbol.style.zIndex = '9999';
 
   // eyeSymbol.style.color = 'white';
   // eyeSymbol.style.textShadow = '0 0 5px black';
@@ -26,8 +26,8 @@ function addEyeSymbol(link) {
 }
 
 function scanForPDFLinks() {
-  const pdfLinks = Array.from(document.querySelectorAll('a[href$=".pdf"]'));
-
+  const pdfLinks = Array.from(document.querySelectorAll('a[download$=".pdf"][target="_blank"]'));
+  
   console.log("here, and scanned")
   console.log(pdfLinks)
   pdfLinks.forEach(link => {
@@ -48,3 +48,18 @@ function scanForPDFLinks() {
 
 
 scanForPDFLinks();
+
+// Set up a MutationObserver to watch for changes in the DOM
+const observer = new MutationObserver(() => {
+  // Call your function to scan for PDF links whenever the DOM changes
+  scanForPDFLinks();
+});
+
+// Define the options for the observer (e.g., observe changes to the entire document)
+const observerOptions = {
+  childList: true,
+  subtree: true,
+};
+
+// Start observing the DOM with the specified options
+observer.observe(document.body, observerOptions);
