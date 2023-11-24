6a5d8e8ea7425657ad0e66d76eb127b633a2c669
Harsh Buddhdev <harshbuddhdev5@.com>
2023-11-24 00:32:13 -0500

    Linkedin completed.

--- popup.js
+++ popup.js
@@ -1,51 +1,58 @@
-
-
 function addEyeSymbol(link) {
-  const eyeSymbol = document.createElement('div');
-  eyeSymbol.innerHTML = '👁️'; // You can customize the symbol
-  eyeSymbol.style.position = 'absolute';
+  const existingEyeSymbol = document.getElementById('LinkedIn_pdf_viwer');
+  
+  // Check if the eye symbol already exists
+  if (!existingEyeSymbol) {
+    const eyeSymbol = document.createElement('div');
+    eyeSymbol.id = 'LinkedIn_pdf_viwer';
+    eyeSymbol.innerHTML = '👁️'; // You can customize the symbol
+    eyeSymbol.style.position = 'absolute';
 
-  // Adjust the position a bit higher (e.g., 10 pixels)
-  // eyeSymbol.style.top = link.offsetTop - 10+ 'px';
+    // Adjust the position a bit higher (e.g., 10 pixels)
+    // eyeSymbol.style.top = link.offsetTop - 10 + 'px';
 
-  eyeSymbol.style.left = link.offsetLeft + link.offsetWidth + 10 + 'px';
-  eyeSymbol.style.fontSize = '20px';
-  eyeSymbol.style.cursor = 'pointer';
-  eyeSymbol.style.zIndex = '9999';
+    eyeSymbol.style.left = link.offsetRight + link.offsetWidth + 10 + 'px';
+    eyeSymbol.style.fontSize = '20px';
+    eyeSymbol.style.cursor = 'pointer';
+    eyeSymbol.style.zIndex = '9999';
 
-  // eyeSymbol.style.color = 'white';
-  // eyeSymbol.style.textShadow = '0 0 5px black';
+    // eyeSymbol.style.color = 'white';
+    // eyeSymbol.style.textShadow = '0 0 5px black';
 
-  eyeSymbol.addEventListener('click', function () {
-    console.log("in the main window")
-    console.log("here", link.textContent)
-    window.open(link.href, '_blank');
-  });
+    eyeSymbol.addEventListener('click', function () {
+      window.open(link.href, '_blank');
+    });
 
-  link.parentElement.appendChild(eyeSymbol);
+    link.parentElement.appendChild(eyeSymbol);
+
+  }
+}
+
+function removeEyeSymbol() {
+  const removeDiv = document.getElementById('LinkedIn_pdf_viwer');
+  if (removeDiv) {
+    setTimeout(function () {
+      removeDiv.remove();
+    }, 800);
+  }
 }
 
 function scanForPDFLinks() {
   const pdfLinks = Array.from(document.querySelectorAll('a[download$=".pdf"][target="_blank"]'));
-  
+
   console.log("here, and scanned")
   console.log(pdfLinks)
   pdfLinks.forEach(link => {
     link.addEventListener('mouseenter', function () {
       addEyeSymbol(link);
     });
-
     link.addEventListener('mouseleave', function () {
-      const eyeSymbol = link.parentElement.querySelector('div');
-      if (eyeSymbol) {
-        setTimeout(function () {eyeSymbol.remove();}, 3000);
-      }
+      removeEyeSymbol();
     });
   });
 
   chrome.runtime.sendMessage({ pdfLinks: pdfLinks.map(link => link.textContent) });
 }
-
 
 scanForPDFLinks();
 
@@ -58,7 +65,7 @@ const observer = new MutationObserver(() => {
 // Define the options for the observer (e.g., observe changes to the entire document)
 const observerOptions = {
   childList: true,
-  subtree: true,
+  subtree: true,   
 };
 
 // Start observing the DOM with the specified options
